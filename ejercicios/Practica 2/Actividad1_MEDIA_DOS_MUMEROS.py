6e13fbb163a4dc8fa692ca4d03a63842d398d296
ccarjim2909 <bleid_cc@hotmail.com>
2024-11-15 12:22:08 +0100

    Segunda version carpeta ejercicios

--- ejercicios/Practica 2/Actividad1_MEDIA_DOS_MUMEROS.py
+++ ejercicios/Practica 2/Actividad1_MEDIA_DOS_MUMEROS.py
@@ -7,3 +7,5 @@ N = float(input ("Escriba un numero: "))
 X = float(input ("Escriba otro numero: "))
 M = (N+X)/2
 print ("La media aritmetica de ",N, "y ",X, "es ",M)
+
+#Esto es un archivo modificado
